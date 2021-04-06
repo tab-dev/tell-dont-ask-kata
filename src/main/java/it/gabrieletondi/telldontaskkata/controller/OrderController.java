d6406c2330c7faf2a7a75f83e12f93758843255e
tab-dev <tabdevelopment1@gmail.com>
2021-04-06 16:15:23 -0500

    Add New Controller to Consoladate Use Case Controllers
    The use case classes are essentially the drivers/controllers for
    the application. This new controller condenses the responsibility
    to one class.

---
+++ src/main/java/it/gabrieletondi/telldontaskkata/controller/OrderController.java
@@ -0,0 +1,46 @@
+package it.gabrieletondi.telldontaskkata.controller;
+
+import it.gabrieletondi.telldontaskkata.domain.exception.UnknownProductException;
+import it.gabrieletondi.telldontaskkata.domain.order.Order;
+import it.gabrieletondi.telldontaskkata.domain.order.OrderItem;
+import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
+import it.gabrieletondi.telldontaskkata.service.OrderService;
+import it.gabrieletondi.telldontaskkata.useCase.SellItemsRequest;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class OrderController {
+
+    private final OrderService orderService;
+    private final ProductCatalog productCatalog;
+
+    public OrderController(OrderService orderService, ProductCatalog productCatalog) {
+        this.orderService = orderService;
+        this.productCatalog = productCatalog;
+    }
+
+    public Order create(SellItemsRequest request){
+        List<OrderItem> orderItems = request.getRequests().stream()
+                .map(itemRequest -> new OrderItem(
+                        Optional.ofNullable(productCatalog.getByName(itemRequest.getProductName()))
+                                .orElseThrow(() -> new UnknownProductException()),
+                        itemRequest.getQuantity()))
+                .collect(Collectors.toList());
+
+        return orderService.create(orderItems);
+    }
+
+    public void approve(int orderId){
+        orderService.approve(orderId);
+    }
+
+    public void reject(int orderId){
+        orderService.reject(orderId);
+    }
+
+    public void ship(int orderId){
+        orderService.ship(orderId);
+    }
+}
---
+++ src/test/java/it/gabrieletondi/telldontaskkata/controller/OrderControllerTest.java
@@ -0,0 +1,89 @@
+package it.gabrieletondi.telldontaskkata.controller;
+
+import it.gabrieletondi.telldontaskkata.domain.Category;
+import it.gabrieletondi.telldontaskkata.domain.Product;
+import it.gabrieletondi.telldontaskkata.domain.exception.UnknownProductException;
+import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
+import it.gabrieletondi.telldontaskkata.service.OrderService;
+import it.gabrieletondi.telldontaskkata.useCase.SellItemRequest;
+import it.gabrieletondi.telldontaskkata.useCase.SellItemsRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.doReturn;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderControllerTest {
+
+    @InjectMocks
+    private OrderController orderController;
+
+    @Mock
+    private OrderService orderService;
+
+    @Mock
+    private ProductCatalog productCatalog;
+
+    @Before
+    public void setUp() throws Exception {
+        Category food = new Category("food", BigDecimal.valueOf(10));
+        doReturn(new Product("salad",
+                new BigDecimal("3.56"),
+                food)).when(productCatalog).getByName("salad");
+        doReturn(new Product("tomato",
+                new BigDecimal("4.65"),
+                food)).when(productCatalog).getByName("tomato");
+    }
+
+    @Test
+    public void sellMultipleItems() throws Exception {
+        SellItemRequest saladRequest = new SellItemRequest();
+        saladRequest.setProductName("salad");
+        saladRequest.setQuantity(2);
+
+        SellItemRequest tomatoRequest = new SellItemRequest();
+        tomatoRequest.setProductName("tomato");
+        tomatoRequest.setQuantity(3);
+
+        final SellItemsRequest request = new SellItemsRequest();
+        request.setRequests(new ArrayList<>());
+        request.getRequests().add(saladRequest);
+        request.getRequests().add(tomatoRequest);
+
+        orderController.create(request);
+
+    }
+
+    @Test(expected = UnknownProductException.class)
+    public void unknownProduct() throws Exception {
+        SellItemsRequest request = new SellItemsRequest();
+        request.setRequests(new ArrayList<>());
+        SellItemRequest unknownProductRequest = new SellItemRequest();
+        unknownProductRequest.setProductName("unknown product");
+        request.getRequests().add(unknownProductRequest);
+
+        orderController.create(request);
+    }
+
+    @Test
+    public void approvedExistingOrder() throws Exception {
+        orderController.approve(1);
+    }
+
+    @Test
+    public void rejectedExistingOrder() throws Exception {
+        orderController.reject(1);
+    }
+
+    @Test
+    public void shipApprovedOrder() throws Exception {
+        orderController.ship(1);
+    }
+}
